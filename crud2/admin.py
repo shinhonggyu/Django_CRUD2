14984534b0bb7017b3cb49ad6d63cd6adec0bdbc
honggyu <zowoz0@naver.com>
2020-07-27 22:03:40 +0900

    admin customize

--- crud2/admin.py
+++ crud2/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'created_at', 'updated_at']
+    list_display_links = ['description']
