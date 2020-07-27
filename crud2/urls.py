7990777db75baf56a7200b7ea10a421c9b34f9d4
honggyu <zowoz0@naver.com>
2020-07-27 15:09:13 +0900

    post_detail_FBV

--- crud2/urls.py
+++ crud2/urls.py
@@ -6,4 +6,5 @@ app_name = 'crud2'
 urlpatterns = [
     path('new/', views.post_new, name='post_new'),
     path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- crud2/views.py
+++ crud2/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse
 from .models import Post
 from .forms import PostForm
 
@@ -21,4 +22,11 @@ def post_list(request):
     qs = Post.objects.all()  # QuerySet
     return render(request, 'crud2/post_list.html', {
         'post_list': qs,
+    })
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'crud2/post_detail.html', {
+        'post': post,
     })
